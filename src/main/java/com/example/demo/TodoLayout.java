433d76158636d1025fc9cf1b87d7054b78c395da
Fabusuyi012 <fabgraphicsng@gmail.com>
2018-07-26 00:11:04 +0100

    Initial commit

--- src/main/java/com/example/demo/TodoLayout.java
+++ src/main/java/com/example/demo/TodoLayout.java
@@ -1,21 +1,25 @@
 package com.example.demo;
 
 import com.vaadin.spring.annotation.SpringComponent;
+import com.vaadin.spring.annotation.UIScope;
 import com.vaadin.ui.VerticalLayout;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 
+@UIScope
 @SpringComponent
 public class TodoLayout extends VerticalLayout {
 
 
     @Autowired
     TodoRepository repo;
+    private List<Todo> todos;
 
     @PostConstruct
     void init() {
+        setWidth("80%");
         update();
     }
 
@@ -24,8 +28,8 @@ public class TodoLayout extends VerticalLayout {
     }
 
     public void setTodos(List<Todo> todos) {
+        this.todos = todos;
         removeAllComponents();
-
         todos.forEach(todo -> addComponent(new TodoItemLayout(todo)));
     }
 
